4b121871c77995c71a8f4103348c736b71875c63
Andrey Santos <Andrey.profissional1821@gmail.com>
2022-04-26 23:06:09 -0300

    implementando QueryMethods e utilizando Keywords

--- src/main/java/com/treinamento/inicialAPI/Controller/RestauranteController.java
+++ src/main/java/com/treinamento/inicialAPI/Controller/RestauranteController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -72,15 +73,9 @@ public class RestauranteController {
 	
 	@PatchMapping("/{RestauranteId}")
 	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos){
-		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
 		
-		if(restauranteAtual == null) {
-			return ResponseEntity.notFound().build();
-		}
-		
-		merge(campos, restauranteAtual);
-		
-		return atualizar(restauranteId, restauranteAtual);
+		return ((HeadersBuilder<?>) ResponseEntity.ok(restauranteId)).build();
 		
 	}
 
--- src/main/java/com/treinamento/inicialAPI/domain/model/repository/RestauranteRepository.java
+++ src/main/java/com/treinamento/inicialAPI/domain/model/repository/RestauranteRepository.java
@@ -1,5 +1,8 @@
 package com.treinamento.inicialAPI.domain.model.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +11,7 @@ import com.treinamento.inicialAPI.domain.model.Restaurante;
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
-
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
 	
 	
